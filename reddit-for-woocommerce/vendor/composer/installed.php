09f2ddddf491337e5009962aa65d630dec442d46
Sean Murphy <sean@iamseanmurphy.com>
2026-08-19 13:53:35 +0000

    1.0.5

--- reddit-for-woocommerce/vendor/composer/installed.php
+++ reddit-for-woocommerce/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'woocommerce/reddit-for-woocommerce',
-        'pretty_version' => 'dev-release/1.0.4',
-        'version' => 'dev-release/1.0.4',
-        'reference' => 'c1a6f06af53637ff7c8adf6246d7eba431e9d763',
+        'pretty_version' => 'dev-release/1.0.5',
+        'version' => 'dev-release/1.0.5',
+        'reference' => 'ca11e32ea25c76826a1c392bce98fbd1d2e4412e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'woocommerce/reddit-for-woocommerce' => array(
-            'pretty_version' => 'dev-release/1.0.4',
-            'version' => 'dev-release/1.0.4',
-            'reference' => 'c1a6f06af53637ff7c8adf6246d7eba431e9d763',
+            'pretty_version' => 'dev-release/1.0.5',
+            'version' => 'dev-release/1.0.5',
+            'reference' => 'ca11e32ea25c76826a1c392bce98fbd1d2e4412e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
